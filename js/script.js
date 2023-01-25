8f1880b1ec2847974b990b15be3c8e3b17cd1ce4
Diji Durojaiye <akindijijaiyejaiye@gmail.com>
2023-01-25 18:44:04 +0100

    add user interface

--- js/script.js
+++ js/script.js
@@ -33,4 +33,23 @@ AddressBook.prototype.deleteContact = function(id) {
 function Contact(firstName, lastName, phoneNumber) {
   this.firstName = firstName;
   this.lastName = lastName;
-  this.phoneNumber = phoneNumber;
+  this.phoneNumber = phoneNumber;};
+
+  Contact.prototype.fullName = function() {
+    return this.firstName + " " + this.lastName;
+  };
+
+  // User Interface Logic ---------
+let addressBook = new AddressBook();
+
+$(document).ready(function() {
+  $("form#new-contact").submit(function(event) {
+    event.preventDefault();
+    const inputtedFirstName = $("input#new-first-name").val();
+    const inputtedLastName = $("input#new-last-name").val();
+    const inputtedPhoneNumber = $("input#new-phone-number").val();
+    let newContact = new Contact(inputtedFirstName, inputtedLastName, inputtedPhoneNumber);
+    addressBook.addContact(newContact);
+    console.log(addressBook.contacts);
+  });
+});
